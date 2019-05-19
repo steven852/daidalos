bb55ff7c001f95b602ab871499d000363edff40e
SerafeimV <serafeimvozis@gmail.com>
2019-05-19 19:22:43 +0300

    Eclipse project
    Με τα αρχεία του στέφανου και φτιαγμένα ονόματα

---
+++ tmp/src/templates/BlankDocumentTemplate.java
@@ -0,0 +1,11 @@
+package templates;
+
+public class BlankDocumentTemplate extends Template {
+	public BlankDocumentTemplate(){
+		super("");
+	}
+	
+	public String getTypeOfDocument() {
+		return "Blank";
+	}
+}
---
+++ tmp/src/templates/FileSaver.java
@@ -0,0 +1,20 @@
+package templates;
+
+import java.io.*;
+
+public class FileSaver {
+	private File targetFile;
+	private BufferedWriter fileWriter;
+	
+	public FileSaver(String fileName) throws IOException{
+		targetFile = new File(fileName + ".tex");
+		targetFile.createNewFile();
+		fileWriter = new BufferedWriter(new FileWriter(targetFile));
+	}
+	
+	public void saveFile(String content) throws IOException{
+		fileWriter.write(content);
+		fileWriter.flush();
+		fileWriter.close();
+	}
+}
